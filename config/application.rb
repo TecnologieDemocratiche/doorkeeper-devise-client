3c84cb5eaddfd5bbd3993b3a6f95886625ffd3e6
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-08-06 12:39:54 +0200

    removed thin script

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+
+Bundler.require(*Rails.groups)
 
 module DoorkeeperDeviseClient
   class Application < Rails::Application
